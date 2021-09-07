efe4e7213ccf5cb7d5adee486b15729f45fbdf3e
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-06 20:27:27 -0400

    Add files via upload

--- AjaxAndJSON/app.js
+++ AjaxAndJSON/app.js
@@ -1,1 +1,37 @@
+document.getElementById('button').addEventListener('click', loadData);
 
+function loadData() {
+    // Create an XHR object
+    const xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onload = function(){
+        if(this.status === 200) {
+            console.log(this.responseText)
+        }
+
+    }
+    xhr.onreadystatechange = function() {   
+        if(this.status === 200 && this.readyState === 4){
+            console.log(this.responseText)
+    }
+    }
+
+    xhr.send();
+
+
+    // ReadtState values
+        // 0: request not initialized
+        // 1: server connection established
+        // 2: request received
+        // 3: processing request
+        // 4: request finished and response is ready
+
+    
+    // HTTP status
+    // 200: "ok"
+    // 403: "Forbidden"
+    // 404: "Not Found"
+}
